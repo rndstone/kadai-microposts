a7de5e350257a7bd8745892d5748f64ba9510495
rndstone <dorppers@gmail.com>
2018-12-01 13:14:30 +0000

    modify model names

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,11 @@ class User < ApplicationRecord
   has_many :microposts
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
-  has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
-  has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :reverses_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverses_of_relationships, source: :user
   
   has_many :favorites
-  has_many :likes, through: :favorites, source: :micropost
+  has_many :microposts_liked, through: :favorites, source: :micropost
   
   def follow(other_user)
     unless self == other_user
